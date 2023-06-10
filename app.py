acd05d0ccbb2f2d6b80f075ddfbbe79d23e20c16
Polycarbon <thirapongpdg@gmail.com>
2023-06-11 00:32:14 +0700

    add visualize section

--- app.py
+++ app.py
@@ -9,7 +9,9 @@ from promp_generator import PromptGenerator, count_tokens
 from services.obs import write_log
 from services.openai import get_completion
 from utils import get_session_id
-
+import plotly.graph_objects as go
+from plotly.subplots import make_subplots
+import pandas as pd
 RELEASE = st.secrets.get("RELEASE", True)
 
 if RELEASE:
@@ -19,11 +21,12 @@ if RELEASE:
 else:
     demo_endpoints = {"hrgpt-api": "http://127.0.0.1:8000/chat/completions",
                       "marketgpt-api": "http://127.0.0.1:7000/chat/completions",
-                      "pi-marketgpt-api": "http://127.0.0.1:6000/chat/completions"}
+                      "pi-marketgpt-api": "http://127.0.0.1:8000/chat/completions"}
 
 
 async def generate_response(demo, chat_logs):
     usage = None
+    generated_visualization = None
     if demo == 'hrgpt':
         question = chat_logs[-1]['content']
         prompt_gen = PromptGenerator()
@@ -47,13 +50,15 @@ async def generate_response(demo, chat_logs):
         response = requests.post(demo_endpoints[demo], json=payload)
         data = response.json()
         generated_content = data['message']['content']
+        generated_visualization = data.get('visualization', None)
         created = data['created']
         usage = data['usage']
     # write session_id, created, role , content, docs
     session_id = get_session_id()
     # session_id = 'asd'
     assistant_log = {'session_id': session_id, 'timestamp': created, 'role': 'assistant',
-                     'content': generated_content, 'usage': usage, 'feed_back': None, 'app': demo}
+                     'content': generated_content, 'visualization': generated_visualization,
+                     'usage': usage, 'feed_back': None, 'app': demo}
     # print(assistant_log)
     if RELEASE:
         write_log(assistant_log, len(chat_logs) + 1)
@@ -102,6 +107,33 @@ def main():
             is_user = (log['role'] == 'user')
             avatar = 'big-smile' if is_user else 'bottts'
             feed_back = message(log['content'], is_user=is_user, key=str(i), avatar_style=avatar)
+            if not is_user and log['visualization'] is not None:
+                if log['visualization']['type'] == 'candlestick':
+                    data = log['visualization']['data']
+                    name = log['visualization']['name']
+                    fig = make_subplots(rows=2, cols=1, shared_xaxes=True,
+                                        vertical_spacing=0.03, subplot_titles=(name, 'Volume'),
+                                        row_width=[0.2, 0.7])
+
+                    # Plot OHLC on 1st row
+                    fig.add_trace(go.Candlestick(x=pd.to_datetime(data['x'], unit='s'),
+                                                 open=data['y'][0],
+                                                 high=data['y'][1],
+                                                 low=data['y'][2],
+                                                 close=data['y'][3],
+                                                 name=name),
+                                  row=1, col=1
+                                  )
+                    # indicators
+                    for indicator, values in zip(data['yaxis'][6:], data['y'][6:]):
+                        fig.add_trace(go.Scatter(x=pd.to_datetime(data['x'], unit='s'), y=values, mode='lines', name=indicator)
+                                      , row=1, col=1)
+                    # Bar trace for volumes on 2nd row without legend
+                    fig.add_trace(go.Bar(x=pd.to_datetime(data['x'], unit='s'), y=data['y'][4], showlegend=False), row=2, col=1)
+                    fig.update(layout_xaxis_rangeslider_visible=False)
+                    st.plotly_chart(fig, theme="streamlit")
+                else:
+                    st.json(log['visualization'], expanded=False)
             if not is_user and feed_back != log['feed_back']:
                 log['feed_back'] = feed_back
                 write_log(log, i)
